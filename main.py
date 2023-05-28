6606cf029af732024f07a4e7377a74e86bbfcbcc
z.ahmadi <ahmadi.zahra395@gmail.com>
2023-05-28 15:37:45 +0330

    transfer endpoint is added

--- main.py
+++ main.py
@@ -95,7 +95,47 @@ async def withdraw(wallet_id: int, wallet_update: WalletUpdateRequest, response:
 
 
 @app.get("/wallets/{wallet_id}/history")
-def get_wallet_history(wallet_id: int) -> List[HistoryTransactionResponse]:
+async def get_wallet_history(wallet_id: int) -> List[HistoryTransactionResponse]:
     with Session(engine) as session:
         transactions = session.query(HistoryTransaction).filter(HistoryTransaction.wallet_id == wallet_id).all()
         return transactions
+
+
+@app.get("/wallets/{source_wallet_id}/transfer/{destination_wallet_id}/{amount}", status_code=status.HTTP_200_OK)
+async def transfer_money(source_wallet_id: int, destination_wallet_id: int, amount: float, response: Response):
+    with Session(engine) as session:
+        source_wallet = session.get(Wallet, source_wallet_id)
+        destination_wallet = session.get(Wallet, destination_wallet_id)
+
+        if not source_wallet or not destination_wallet:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"error": "Wallet not found"}
+
+        if source_wallet.balance < amount:
+            response.status_code = status.HTTP_406_NOT_ACCEPTABLE
+            return {"error": "Insufficient balance"}
+
+        source_wallet.balance -= amount
+        destination_wallet.balance += amount
+
+        # Save the changes in the wallets
+        session.add(source_wallet)
+        session.add(destination_wallet)
+        session.commit()
+
+        # history transaction records
+        source_transaction = HistoryTransaction(
+            wallet_id=source_wallet_id,
+            transaction_type="withdraw",
+            amount=amount
+        )
+        destination_transaction = HistoryTransaction(
+            wallet_id=destination_wallet_id,
+            transaction_type="deposit",
+            amount=amount
+        )
+        session.add(source_transaction)
+        session.add(destination_transaction)
+        session.commit()
+
+        return {"message": "Money transferred successfully"}
